57adc9fe5f48977f5b46865b20eca1e856ef158e
alfredo.salzillo <noctisbels@gmai.com>
2019-08-10 14:48:27 +0200

    removed useless stories

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,10 +1,5 @@
-import {
-  storiesOf,
-  html,
-} from '@open-wc/demoing-storybook';
-import { action } from '@storybook/addon-actions';
+import { html, storiesOf, } from '@open-wc/demoing-storybook';
 import { component, useState } from 'haunted/lib/haunted';
-import { css, string } from '../src/css';
 import styled from '../src/main';
 
 const StyledButton = styled.button`
@@ -35,35 +30,16 @@ const StyledDiv = styled.div`
   width: 100vh;
   height: 100vw;
   margin: 100px;
-  background: gray; 
+  background: gray;
 `;
 customElements.define('styled-div', StyledDiv, {
   extends: 'div',
 });
 
-storiesOf('css template', module)
-  .add(
-    'test generator',
-    () => {
-      const className = css`
-         color: black;
-         width: 100px;
-         height: ${100}px;
-      `;
-      return html`
-        <div class=${className}>
-          <code>
-            ${string()}
-          </code>        
-        </div>
-      `;
-    },
-  )
+storiesOf('Styled element', module)
   .add(
     'button',
     () => html`
-        <!-- <button is="styled-button" @click=${action('click')}>Test Button</button>
-        <button is="styled-button" @click=${action('click-disabled')} disabled>Test Button</button></-->
         <test-button>Click to disable</test-button>
       `,
   )
