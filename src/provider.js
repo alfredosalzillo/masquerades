16fa4e11579faa7322f107147470a30b54d78a5c
alfredo salzillo <alfredo.salzillo@progettopa.it>
2019-08-21 12:13:39 +0200

    fixed event not fired on value change

--- src/provider.js
+++ src/provider.js
@@ -15,19 +15,31 @@ export const valueOf = (identifier, context) => {
   const theNearestProvider = providerOf(identifier, context);
   return theNearestProvider && theNearestProvider.value;
 };
+const values = new WeakMap();
 export const createProvider = (
   initialValue,
   identifier = Symbol(`provider-${Date.now()}`),
 ) => {
   class Provider extends HTMLElement {
+    // TODO FIX: why this is not working?
+    static get observedAttributes() {
+      return ['value'];
+    }
+
+    // TODO: remove get and setter when fixed observedAttributes
+    set value(value) {
+      this.notifyChanges();
+      values.set(this, value);
+    }
+
+    get value() {
+      return values.get(this);
+    }
+
     constructor() {
       super();
       this.value = initialValue;
       this[providerSymbol] = identifier;
-    }
-
-    static get observedAttributes() {
-      return ['value'];
     }
 
     static of(context) {
@@ -36,8 +48,12 @@ export const createProvider = (
 
     attributeChangedCallback(name) {
       if (name === 'value') {
-        this.dispatchEvent(new Event('provider-value-change'));
+        this.notifyChanges();
       }
+    }
+
+    notifyChanges() {
+      this.dispatchEvent(new Event('provider-value-change'));
     }
   }
   return Provider;
